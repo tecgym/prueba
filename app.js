4dca54eae945f4f7cb89a837a107e874712e5b74
tecgym <tecgymcolombiana@gmail.com>
2021-08-18 01:45:56 -0500

    mi primer commit

---
+++ app.js
@@ -0,0 +1,9 @@
+function lista(value) {
+    switch (value) {
+        case 1: alert("Selecciona una tarjeta ");
+            break;
+        case 2: alert("escogiste 460 athletic");
+            break;
+    }
+}
+
---
+++ ej.cs
@@ -0,0 +1,15 @@
+int edad=2;
+
+switch (edad)
+{
+    case 1: 
+      Console.WriteLine("UNO");
+      break;
+      case 2:
+      Console.WriteLine("DOS");
+      case 3:
+      Console.WriteLine("TRES");
+     default:
+      Consele.WriteLine("FAVOR ESCRIBIR UN NUMERO DEL 1 AL 3");
+
+}
